3879116feb7b0f5382164fcafe02a9f4201387b0
Nigel Brookes-Thomas <nigel.brookes-thomas@dvla.gov.uk>
2024-08-19 09:00:18 +0100

    premerge

--- app/controllers/milestones_controller.rb
+++ app/controllers/milestones_controller.rb
@@ -26,7 +26,7 @@ class MilestonesController < ApplicationController
 
     respond_to do |format|
       if @milestone.save
-        format.html { redirect_to project_milestone_url(@project, @milestone), notice: 'Milestone was successfully created.' }
+        format.html { redirect_to project_url(@project), notice: "Milestone was successfully created." }
         format.json { render :show, status: :created, location: @milestone }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -39,7 +39,7 @@ class MilestonesController < ApplicationController
   def update
     respond_to do |format|
       if @milestone.update(milestone_params)
-        format.html { redirect_to project_milestone_url(@project, @milestone), notice: 'Milestone was successfully updated.' }
+        format.html { redirect_to project_url(@project), notice: "Milestone was successfully updated." }
         format.json { render :show, status: :ok, location: @milestone }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -53,7 +53,7 @@ class MilestonesController < ApplicationController
     @milestone.destroy!
 
     respond_to do |format|
-      format.html { redirect_to project_url(@project), notice: 'Milestone was successfully destroyed.' }
+      format.html { redirect_to project_url(@project), notice: "Milestone was successfully destroyed." }
       format.json { head :no_content }
     end
   end
