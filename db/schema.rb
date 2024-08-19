5c5c8dc779ed65a5a3d474bea69c89db2c5ab27d
Nigel Brookes-Thomas <nigel.brookes-thomas@dvla.gov.uk>
2024-08-19 09:41:28 +0100

    remove end_date from milestones

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_08_16_130125) do
+ActiveRecord::Schema[7.2].define(version: 2024_08_19_083630) do
   create_table "milestones", force: :cascade do |t|
     t.string "name"
     t.date "start_date"
