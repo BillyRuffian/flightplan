e9af939fb6e296d6219cea4fc222f7666aec577b
Nigel Brookes-Thomas <nigel.brookes-thomas@dvla.gov.uk>
2024-08-23 08:14:31 +0100

    cops

--- app/jobs/reminder_due_job.rb
+++ app/jobs/reminder_due_job.rb
@@ -2,7 +2,7 @@ class ReminderDueJob < ApplicationJob
   queue_as :default
 
   def perform(*args)
-    puts "Checking for reminders due today"
+    puts 'Checking for reminders due today'
     Reminder.where(start_date: Date.today, notified: false).each do |reminder|
       ReminderMailer.reminder_email(reminder).deliver_later
       reminder.notified = true
