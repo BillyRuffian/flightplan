880f87950db9ec70f3b3d25ec28e20fc270738ed
Nigel Brookes-Thomas <nigel.brookes-thomas@dvla.gov.uk>
2024-08-23 08:33:18 +0100

    cops

--- test/controllers/reminders_controller_test.rb
+++ test/controllers/reminders_controller_test.rb
@@ -1,45 +1,45 @@
-require "test_helper"
+require 'test_helper'
 
 class RemindersControllerTest < ActionDispatch::IntegrationTest
   setup do
     @reminder = reminders(:one)
   end
 
-  test "should get index" do
+  test 'should get index' do
     get reminders_url
     assert_response :success
   end
 
-  test "should get new" do
+  test 'should get new' do
     get new_reminder_url
     assert_response :success
   end
 
-  test "should create reminder" do
-    assert_difference("Reminder.count") do
+  test 'should create reminder' do
+    assert_difference('Reminder.count') do
       post reminders_url, params: { reminder: { project_id: @reminder.project_id, start_date: @reminder.start_date, topic: @reminder.name } }
     end
 
     assert_redirected_to reminder_url(Reminder.last)
   end
 
-  test "should show reminder" do
+  test 'should show reminder' do
     get reminder_url(@reminder)
     assert_response :success
   end
 
-  test "should get edit" do
+  test 'should get edit' do
     get edit_reminder_url(@reminder)
     assert_response :success
   end
 
-  test "should update reminder" do
+  test 'should update reminder' do
     patch reminder_url(@reminder), params: { reminder: { project_id: @reminder.project_id, start_date: @reminder.start_date, topic: @reminder.name } }
     assert_redirected_to reminder_url(@reminder)
   end
 
-  test "should destroy reminder" do
-    assert_difference("Reminder.count", -1) do
+  test 'should destroy reminder' do
+    assert_difference('Reminder.count', -1) do
       delete reminder_url(@reminder)
     end
 
